08ede13010e4c5f6d749f6329a919e73f2e58ae3
Candace Grant <cgrantrock@gmail.com>
2016-02-29 18:47:59 -0500

    Task 4 is completed

---
+++ task_04.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This docstring creates a for loop"""
+
+def process_data(data):
+    for n in data[:]:
+        average = total / len(data)
+        print ('total', 'average')
