095a70fd2d08d3bd16e5f769b185dd16d6fe83ee
paisely65 <cunysps@gmail.com>
2016-02-29 19:30:59 -0500

    Task_01 has been revised based on Jenkin's comment

--- task_01.py
+++ task_01.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """This docstring compares lists and tuples."""
 
-ELEMENTS = ('None', 'Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron', 'Carbon')
+ELEMENTS = (None, 'Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron', 'Carbon')
 OPERATIONS_ORDER = ['Parentheses', 'Exponents', 'Multiplication', 'Division', 'Addition', 'Subtraction']
 print ELEMENTS
 print OPERATIONS_ORDER
