37861e14c85c982d5eb57a606513c3ba27efdcb0
paisely65 <cunysps@gmail.com>
2016-03-03 14:22:54 -0500

    Another attempt to format line length

--- task_01.py
+++ task_01.py
@@ -3,8 +3,8 @@
 """This docstring compares lists and tuples."""
 
 ELEMENTS = (None, 'Hydrogen', 'Helium', 'Lithium', 'Beryllium',
-        'Boron', 'Carbon')
+            'Boron', 'Carbon')
 OPERATIONS_ORDER = ['Parentheses', 'Exponents', 'Multiplication',
-        'Division', 'Addition', 'Subtraction']
+                   'Division', 'Addition', 'Subtraction']
 print ELEMENTS
 print OPERATIONS_ORDER
