55e6869043d7a1120ff46c53281f2a3958664be4
paisely65 <cunysps@gmail.com>
2016-03-03 11:47:59 -0500

    I modified task 5 to include the docstring

--- task_05.py
+++ task_05.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""This docstring shows mutability differences"""
+"""This docstring modifies a list"""
+
 
 def flip_keys(to_flip):
+    """This function that shows mutability.
+
+    Args:
+        to_flip(list): The input is a list.
+
+    Returns:
+        list:The original list is reversed.
+
+    Examples:
+        >>>flip_keys(['pepper'], ['salt'], [10, 20, 30])
+
+        >>>flip_keys(['hello','world'])
+
+    """
+    tally = 0
     for item in to_flip:
-        print to_flip[-1:]
+        to_flip[tally] = item[::-1]
+        tally += 1
+
+    return to_flip
+       
 
