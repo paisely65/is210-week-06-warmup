58d4564f9432483d8e2775b771e39f38f5bdc3d0
paisely65 <cunysps@gmail.com>
2016-03-03 14:10:14 -0500

    The lines have been shortened.

--- task_01.py
+++ task_01.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 """This docstring compares lists and tuples."""
 
-ELEMENTS = (None, 'Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron', 'Carbon')
-OPERATIONS_ORDER = ['Parentheses', 'Exponents', 'Multiplication', 'Division', 'Addition', 'Subtraction']
+ELEMENTS = (None, 'Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron',\
+           'Carbon')
+OPERATIONS_ORDER = ['Parentheses', 'Exponents', 'Multiplication', 'Division',\
+                   'Addition', 'Subtraction']
 print ELEMENTS
 print OPERATIONS_ORDER
-
